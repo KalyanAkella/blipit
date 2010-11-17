f4668069cef3322a471637b545b41c817eeffce6
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-11-17 10:52:07 +0000

    Making the test move to the test package
    git-svn-id: https://blipit.googlecode.com/svn/trunk@7 7452eb57-69d3-3901-e9e3-bab7012f460b

--- appengine/blipitclient/test/com/thoughtworks/blipit/TransportLayerImplTest.java
+++ appengine/blipitclient/test/com/thoughtworks/blipit/TransportLayerImplTest.java
@@ -4,12 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TransportLayerImplTest {
+    
     @org.junit.Test
     public void testGetBlipTitle() throws Exception {
         TransportLayerImpl transportLayer = new TransportLayerImpl();
         Assert.assertEquals(transportLayer.getBlipTitle(), "Blip It");  
     }
-
+    
     @Test
     public void testGetBlipSnippet() throws Exception {
         TransportLayerImpl transportLayer = new TransportLayerImpl();
