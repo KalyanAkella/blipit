d867bf46abf4ed948c12aac6a90ca4cf3a441cd8
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-11-17 14:10:14 +0000

    Making the app aware of location
    git-svn-id: https://blipit.googlecode.com/svn/trunk@9 7452eb57-69d3-3901-e9e3-bab7012f460b

--- android/BlipItMain/src/com/thoughtworks/blipit/BlipItActivity.java
+++ android/BlipItMain/src/com/thoughtworks/blipit/BlipItActivity.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.blipit;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -13,6 +17,12 @@ import com.thoughtworks.blipit.overlays.BlipItOverlay;
 import java.util.List;
 
 public class BlipItActivity extends MapActivity {
+    
+    private LocationManager locationManager;
+    private MapView mapView;
+    private OverlayItem blip;
+
+
     /**
      * Called when the activity is first created.
      */
@@ -20,20 +30,23 @@ public class BlipItActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        MapView mapView = (MapView) this.findViewById(R.id.mapview);
+        mapView = (MapView) this.findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         Drawable drawable = this.getResources().getDrawable(R.drawable.marker);
         BlipItOverlay blipItOverlay = new BlipItOverlay(drawable, mapView);
         addBlip(blipItOverlay, mapView.getController());
         List<Overlay> mapOverlays = mapView.getOverlays();
         mapOverlays.add(blipItOverlay);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
     private void addBlip(BlipItOverlay blipItOverlay, MapController mapController) {
         String title = this.getResources().getString(R.string.blip_title);
         String snippet = this.getResources().getString(R.string.blip_snippet);
         GeoPoint geoPoint = new GeoPoint(19240000, -99120000);
-        blipItOverlay.addBlip(new OverlayItem(geoPoint, title, snippet));
+        blip = new OverlayItem(geoPoint, title, snippet);
+        blipItOverlay.addBlip(blip);
         mapController.animateTo(geoPoint);
     }
 
@@ -41,4 +54,25 @@ public class BlipItActivity extends MapActivity {
     protected boolean isRouteDisplayed() {
         return false;
     }
+
+    LocationListener locationListener = new LocationListener(){
+
+        public void onLocationChanged(Location location) {
+            int latitude = (int) (location.getLatitude() * 1E6);
+            int longitude = (int) (location.getLongitude() * 1E6);
+            mapView.getController().animateTo(new GeoPoint(latitude, longitude));
+        }
+
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        public void onProviderEnabled(String s) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        public void onProviderDisabled(String s) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+    };
 }
