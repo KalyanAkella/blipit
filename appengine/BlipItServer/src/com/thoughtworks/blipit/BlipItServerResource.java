20ab86a37fe783db3d161d78f33aa83e7a1d72e7
Kalyan Akella <kalyan.akella@gmail.com>
2010-11-27 08:08:59 +0000

    Added appengine side logging support
    git-svn-id: https://blipit.googlecode.com/svn/trunk@29 7452eb57-69d3-3901-e9e3-bab7012f460b

--- appengine/BlipItServer/src/com/thoughtworks/blipit/BlipItServerResource.java
+++ appengine/BlipItServer/src/com/thoughtworks/blipit/BlipItServerResource.java
@@ -7,7 +7,10 @@ import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
+import java.util.logging.Logger;
+
 public class BlipItServerResource extends ServerResource implements BlipItResource {
+    private static final Logger log = Logger.getLogger(BlipItServerResource.class.getName());
 
     @Get
     public String showMessage() {
@@ -16,8 +19,10 @@ public class BlipItServerResource extends ServerResource implements BlipItResour
 
     @Post
     public BlipItResponse echo(BlipItRequest blipItRequest) {
+        log.info("Blip Request received. Message: " + blipItRequest.getMessage());
         BlipItResponse blipItResponse = new BlipItResponse();
         blipItResponse.setMessage(blipItRequest.getMessage());
+        log.info("Blip Response sent. Message: " + blipItResponse.getMessage());
         return blipItResponse;
     }
 }
