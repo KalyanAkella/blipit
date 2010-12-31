95e3fe79b7c4c30123c611dbd721b4a270423d59
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-12-31 06:31:38 +0000

    Making blipit sms talk to Google's Geocoder Service
    git-svn-id: https://blipit.googlecode.com/svn/trunk@102 7452eb57-69d3-3901-e9e3-bab7012f460b

---
+++ blipitsms/geocoder/handlers/geocoder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
+import urllib2, urllib
+import json
+
+class GeoCoderHandler(KeywordHandler):
+    """
+    Handle any message prefixed ``echo``, responding with the remainder
+    of the text. Useful for remotely testing internationalization.
+    """
+
+    keyword = "panic|fire|PANIC|FIRE"
+
+    def help(self):
+        self.respond("To echo some text, send: PANIC <ANYTHING>")
+
+    def handle(self, text):
+        url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=" + urllib.quote_plus(text)
+        print url
+        result = urllib2.urlopen(url)
+        json_data = json.loads(result.read())
+        print json_data
+        if json_data['status'] == u'OK':
+            latitude = json_data['results'][0]['geometry']['location']['lat']
+            longitude = json_data['results'][0]['geometry']['location']['lng']
+            self.respond("Your lat is :" + latitude + " longitude is :" + longitude)
+            return
+            
+        self.respond("Sorry we do not understand your address!")
+        
