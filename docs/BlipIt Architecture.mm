8944c2b3fe124bd9bbcd107d8bb24b370f12f45c
Kalyan Akella <kalyan.akella@gmail.com>
2011-03-14 06:57:58 +0000

    Adding the presentation & architecture mind map files under the docs folder
    git-svn-id: https://blipit.googlecode.com/svn/trunk@181 7452eb57-69d3-3901-e9e3-bab7012f460b

---
+++ docs/BlipIt Architecture.mm
@@ -0,0 +1,66 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1298727766227" ID="ID_478756741" MODIFIED="1298728594013" TEXT="BlipIt Architecture">
+<font ITALIC="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1298727850112" ID="ID_843376340" MODIFIED="1298728574403" POSITION="right" TEXT="BlipItService">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298728103900" ID="ID_1126684350" MODIFIED="1298728528558" TEXT="RESTful APIs">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728374379" ID="ID_779001573" MODIFIED="1298728528558" TEXT="Uses the Restlet framework">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1298728111834" ID="ID_1186114159" MODIFIED="1298728528558" TEXT="Transport over HTTP JSON">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1298728123546" ID="ID_1933466824" MODIFIED="1298728528558" TEXT="Hosted on Google&apos;s AppSpot">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1298727962762" ID="ID_1958533687" MODIFIED="1298728572660" POSITION="left" TEXT="BlipItClients">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298728214099" ID="ID_733739819" MODIFIED="1298728528557" TEXT="Android">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728039916" ID="ID_151641452" MODIFIED="1298728528557" TEXT="BlipItClient">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728315636" ID="ID_207693834" MODIFIED="1298728528557" TEXT="Used for viewing blips">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1298728047851" ID="ID_1622461383" MODIFIED="1298728528557" TEXT="PanicBlip">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728302065" ID="ID_1701307475" MODIFIED="1298728528557" TEXT="Used for sending Panic blips">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1298728218834" ID="ID_83306526" MODIFIED="1298728528557" TEXT="SMS">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728235930" ID="ID_717561545" MODIFIED="1298728528557" TEXT="PanicSMS">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1298728245248" ID="ID_461456308" MODIFIED="1298728528556" TEXT="Python based">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1298728253446" ID="ID_785972180" MODIFIED="1298728528556" TEXT="Uses RapidSMS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1298728284070" ID="ID_1212937243" MODIFIED="1298728528556" TEXT="Google&apos;s geocoding APIs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1298728266361" ID="ID_1500673386" MODIFIED="1298728528556" TEXT="Used for sending Panic SMS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1298728182466" ID="ID_655081691" MODIFIED="1298728575796" POSITION="right" TEXT="BlipIt Management Console">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298728188957" ID="ID_1484949660" MODIFIED="1298728528555" TEXT="JSP based">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1298728407283" ID="ID_1614804676" MODIFIED="1298728528554" TEXT="Useful for create/delete channels &amp; blips">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
